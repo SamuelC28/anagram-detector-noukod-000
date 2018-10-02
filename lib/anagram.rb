d3b7b42a378f7d051ca5ba0bbe4ca1ce7a06f9a8
Samuel Cornet <samuel@noukod.com>
2018-10-02 20:25:55 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,8 +1,18 @@
 class Anagram
+
   attr_accessor :word
 
-  def match(word)
+  def initialize(word)
     @word = word
   end
 
+  def match(array)
+    arr = []
+    array.each do |anagram|
+      if anagram.split("").sort == @word.split("").sort
+        arr << anagram
+      end
+    end
+    arr
+  end
 end
